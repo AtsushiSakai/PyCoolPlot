375fb2dda4a85a4258b286e7db7b660f7ac104b1
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-06-02 22:06:57 +0900

    first release

--- pycoolplot.py
+++ pycoolplot.py
@@ -18,7 +18,7 @@ from cycler import cycler
 import matplotlib.dates as mdates
 
 
-def horizontal_bar(index, data, color="C0"):
+def horizontal_bar(index, data, color="C0", xlabel="", rate_graph=False):
     """
     Beautiful bar chart
 
@@ -29,26 +29,37 @@ def horizontal_bar(index, data, color="C0"):
     plt.show()
 
     """
+
+    if rate_graph:
+        data = [round(d / sum(data) * 100.0, 1) for d in data]
+
     combatpower = pd.DataFrame(data,
                                index=index,
                                columns=['data'])
 
     fig, ax = plt.subplots()
-    combatpower.plot.barh(legend=False, ax=ax, width=0.8, color=color)
-    ax.invert_yaxis()
     plt.gca().invert_yaxis()
+    ax.invert_yaxis()
     [spine.set_visible(False) for spine in ax.spines.values()]
-    plt.grid(False)
     ax.tick_params(bottom=False, left=False, labelbottom=False)
     ax.tick_params(axis='y', labelsize='x-large')
     vmax = combatpower['data'].max()
+    low_contrast(xlabel, "", ax)
+    combatpower.plot.barh(legend=False, ax=ax, width=0.8, color=color)
     for i, value in enumerate(combatpower['data']):
-        ax.text(value + vmax * 0.02, i,
-                f'{value:,}', fontsize='x-large', color=color)
+        if rate_graph:
+            ax.text(value + vmax * 0.02, i,
+                    f'{value:,}%', fontsize='x-large', color=color)
+        else:
+            ax.text(value + vmax * 0.02, i,
+                    f'{value:,}', fontsize='x-large', color=color)
+
+    plt.grid(False)
 
 
 def line_graph(data, index, columns, xlabel, ylabel,
-               xtick=None, ytick=None, focus_id=None, focus_color='limegreen'):
+               xtick=None, ytick=None, focus_id=None,
+               focus_color='limegreen'):
     """
     Beautiful line graph
 
@@ -69,8 +80,6 @@ def line_graph(data, index, columns, xlabel, ylabel,
                             index=index,
                             columns=columns)
 
-    c = plt.get_cmap('Set1').colors
-    plt.rcParams['axes.prop_cycle'] = cycler(color=c)
     fig, ax = plt.subplots()
     if focus_id is None:
         carsales.T.plot(ax=ax, linewidth=4, legend=False)
@@ -79,7 +88,6 @@ def line_graph(data, index, columns, xlabel, ylabel,
         ax.lines[focus_id].set_color(focus_color)
     year = carsales.columns.values
     ax.set(xlabel=xlabel, ylabel=ylabel, xlim=(year.min(), year.max()))
-    ax.yaxis.label.set_color('gray')
     # adjust tick
     if xtick is not None:
         ax.xaxis.set_major_locator(MultipleLocator(xtick))
@@ -89,12 +97,13 @@ def line_graph(data, index, columns, xlabel, ylabel,
     ax.grid(axis='x')
 
     ax.tick_params(bottom=False, left=False)
-    ax.tick_params(axis='y', colors='gray')
-    ax.tick_params(axis='x', colors='dimgray')
+
     ax.spines['right'].set_visible(False)
     ax.spines['left'].set_visible(False)
     ax.spines['top'].set_visible(False)
-    ax.spines['bottom'].set_color('dimgray')
+
+    low_contrast(xlabel, ylabel, ax)
+
     for i, name in enumerate(carsales.index.values):
         if focus_id is None:
             c, f = f'C{i}', 'large'
@@ -114,15 +123,9 @@ def time_vertical_bar(data, time_index,
     Beautiful time vertical bar
 
     usage:
-    data = [[970, 1010, 1015, 1008],
-            [975, 1020, 1002, 1035],
-            [975, 985, 995, 999]]
-    index = ['Toyota', 'VW', 'GM']
-    columns = [2013, 2014, 2015, 2016]
-    ylabel = "Number"
-    xlabel = "Year"
-    line_graph(data, index, columns, xlabel,
-               ylabel, xtick=1, ytick=25, focus_id=None)
+    data = np.linspace(450, 990, 12) + np.random.randint(-50, 50, 12)
+    time_index = pd.date_range('2017/5', periods=12, freq='MS')
+    time_vertical_bar(data, time_index, xlabel="time", ylabel="MAU")
     plt.show()
 
     """
@@ -134,22 +137,18 @@ def time_vertical_bar(data, time_index,
 
     ax.bar(df.index, df, width=20,
            color='coral', zorder=2, align='center')
-    ax.set_ylabel(ylabel, color='gray')
-    ax.set_xlabel(xlabel, color='gray')
-
     if xtick is not None:
         ax.xaxis.set_major_locator(MultipleLocator(xtick))
     if ytick is not None:
         ax.yaxis.set_major_locator(MultipleLocator(ytick))
 
     ax.tick_params(bottom=False, left=False)
-    ax.tick_params(axis='x', colors='dimgray')
-    ax.tick_params(axis='y', colors='gray')
+
     ax.grid(axis='y')
+    low_contrast(xlabel, ylabel, ax)
     ax.spines['left'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
-    ax.spines['bottom'].set_color('dimgray')
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%-m'))
     ax.xaxis.set_major_locator(mdates.MonthLocator())
     fig.canvas.draw()
@@ -162,23 +161,34 @@ def time_vertical_bar(data, time_index,
                     arrowprops={'arrowstyle': '-', 'color': 'dimgray'})
 
 
+def low_contrast(xlabel, ylabel, ax):
+    ax.set_ylabel(ylabel, color='gray')
+    ax.set_xlabel(xlabel, color='gray')
+    ax.tick_params(axis='x', colors='gray')
+    ax.tick_params(axis='y', colors='gray')
+    ax.spines['left'].set_color('dimgray')
+    ax.spines['right'].set_color('dimgray')
+    ax.spines['top'].set_color('dimgray')
+    ax.spines['bottom'].set_color('dimgray')
+
+
 def main():
     print(__file__ + " start!!")
     print("matplolib version:", matplotlib._version.get_versions()['version'])
 
-    #  data = [1000, 2000, 10000]
-    #  index = ["A", "B", "C"]
-    #  horizontal_bar(index, data)
+    data = [1000, 2000, 10000]
+    index = ["A", "B", "C"]
+    horizontal_bar(index, data, rate_graph=False)
 
-    #  data = [[970, 1010, 1015, 1008],
-    #  [975, 1020, 1002, 1035],
-    #  [975, 985, 995, 999]]
-    #  index = ['Toyota', 'VW', 'GM']
-    #  columns = [2013, 2014, 2015, 2016]
-    #  ylabel = "Number"
-    #  xlabel = "Year"
-    #  line_graph(data, index, columns, xlabel,
-    #  ylabel, xtick=1, ytick=25, focus_id=None)
+    data2 = [[970, 1010, 1015, 1008],
+             [975, 1020, 1002, 1035],
+             [975, 985, 995, 999]]
+    index2 = ['Toyota', 'VW', 'GM']
+    columns = [2013, 2014, 2015, 2016]
+    ylabel = "Number"
+    xlabel = "Year"
+    line_graph(data2, index2, columns, xlabel,
+               ylabel, xtick=1, ytick=25, focus_id=None)
 
     data = np.linspace(450, 990, 12) + np.random.randint(-50, 50, 12)
     time_index = pd.date_range('2017/5', periods=12, freq='MS')
